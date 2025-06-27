b367f4cdad6c8f7dc6007756a9f514804ea789ae
Avelar_0 <00089625@uca.edu.sv>
2025-06-27 17:59:10 -0600

    ya es paja

--- funcionArray.cpp
+++ funcionArray.cpp
@@ -5,16 +5,20 @@ void guardarArray ( int array[], int tam = 5 ){
 
 
     for ( int i = 0; i < tam; i++){
+        cout<<" nota "<< i + 1 <<":" << array[i]<<endl;
 
-        cout<<array[i]<<endl;
-
-       return ;
+      
     }
 
 }
 int main ( ){
-    
-    
+
+    int array[5] = {76,87,98,87,98};
+    cout<<" tus notas son: "<< endl;
+    guardarArray(array);
+
+
+
 
     return 0;
 }
